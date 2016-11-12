8edd17280c996f3fa768a8dfcf6ff3a1cfd3df39
Peter Lu <peterlu6@stanford.edu>
2016-11-12 15:22:44 -0800

    ahhhhhh

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.post("/message", function (request, response) {
   console.log(request.body.Body);
   console.log(request.body.From);  
-  document.getElementById("line").innerHTML("cat");
+  document.getElementById("line").innerHTML=request.body.Body;
   response.send("<Response><Message>" + request.body.Body + "</Message></Response>");
 });
 
