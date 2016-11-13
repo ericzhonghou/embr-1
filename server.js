8e467b6ff52cc143613dedd679dbaf7cd4fcfcba
Eric Hou <ericzhonghou@gmail.com>
2016-11-13 03:29:18 -0800

    combined front + back

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-var express = require('express');
+dvar express = require('express');
+var jsonstr = require('./test.json');
+var jsonfile = require('jsonfile')
 var bodyParser = require('body-parser');
 var NaturalLanguageClassifierV1 = require('watson-developer-cloud/natural-language-classifier/v1');
 var app = express();
@@ -9,7 +11,15 @@ app.post("/message", function (request, response) {
   console.log(request.body.From);
   var clusterJSON = getClassification(request.body.Body);
   console.log(clusterJSON.top_class);
+  var obj = JSON.parse(jsonStr);
 
+  for(i = 0; i < obj['children'].length; i++) {
+  	if(obj['children'][i]['name'] == clusterJSON) {
+  		obj['children'][i]['children'][0]['children'][0]['sms'].push({"textmess": request.body.Body});
+  		obj['children'][i]['children'][0]['children'][0]['size'] += 1;
+  		jsonStr = JSON.stringify(obj);
+  		jsonfile.writeFileSync(./test.json, obj);
+  	} 
 });
 
  
