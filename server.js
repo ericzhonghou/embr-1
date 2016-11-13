3032eb02c8c85e9e93ac5ea9b5d936c53aedec82
Peter Lu <peterlu6@stanford.edu>
2016-11-12 21:34:16 -0800

    added classifier

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.post("/message", function (request, response) {
   console.log(request.body.Body);
   console.log(request.body.From);
-  response.send("<Response><Message>" + request.body.Body + "</Message></Response>");
+  var cluster = getClassification(request.body.Body);
+  console.log(cluster);
+
 });
 
  
@@ -17,3 +19,24 @@ app.get("/", function (request, response) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
+
+function getClassification(query){
+	var NaturalLanguageClassifierV1 = require('watson-developer-cloud/natural-language-classifier/v1');
+
+	var natural_language_classifier = new NaturalLanguageClassifierV1({
+	  username: '78da41dc-3b5e-4e35-a514-0c7bb266d642',
+	  password: '6utZEoLsdqDu'
+	});
+
+	natural_language_classifier.classify({
+	  text: query,
+	  classifier_id: '<classifier-id>' },
+	  function(err, response) {
+	    if (err)
+	      console.log('error:', err);
+	    else
+	      console.log(JSON.stringify(response, null, 2));
+	});
+}
