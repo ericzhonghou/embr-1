32ac807541c7326503faa9196443d0d88d94e8c2
Eric Hou <ericzhonghou@gmail.com>
2016-11-13 10:01:48 -0800

    package

--- server.js
+++ server.js
@@ -29,9 +29,9 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
  
-var listener = app.listen(8000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+// var listener = app.listen(8000, function () {
+//   console.log('Your app is listening on port ' + listener.address().port);
+// });
 
 
 
