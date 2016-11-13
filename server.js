20d60b381cc4f7f7abbaddd5a2d51d8b2c6ae51d
Eric Hou <ericzhonghou@gmail.com>
2016-11-13 04:07:38 -0800

    semicolons

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var jsonstr = require('./test.json');
-var jsonfile = require('jsonfile')
+var jsonfile = require('jsonfile');
 var bodyParser = require('body-parser');
 require('./graph.js')();
 var NaturalLanguageClassifierV1 = require('watson-developer-cloud/natural-language-classifier/v1');
